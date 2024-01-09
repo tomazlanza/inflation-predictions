25e166d6a9088f6999ed373b8698db3b2f3737e3
tomazlanza <lanza.tomaz@gmail.com>
2024-01-09 20:17:01 -0300

    changing vite base  config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/inflation-predictions"
+  base: "/inflation-predictions/dist"
 })
